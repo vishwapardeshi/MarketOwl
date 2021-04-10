4263dec3f75cc5a10b20b6f6036145672fb39016
Vishwa Pardeshi <pardeshi.vishwa25@gmail.com>
2021-04-10 14:38:18 -0400

    Add vader and textblob

--- src/sentiment.py
+++ src/sentiment.py
@@ -1,3 +1,9 @@
+import numpy as np
+
+#for sentiment analysis
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from textblob import TextBlob
+
 class Sentiment:
     def __init__(self, method = 'vader'):
         self.method = method
@@ -6,13 +12,48 @@ class Sentiment:
         """
         Function to get sentiment label using vader library
         """
-        pass
+        vader = SentimentIntensityAnalyzer()
+        df['polarity_score'] = df[text_col].apply(vader.polarity_scores)
+
+        # create a list of our conditions
+        conditions = [
+            (df['polarity_score'] <= -0.05),
+            (df['polarity_score'] > -0.05) & (df['polarity_score'] < 0.05),
+            (df['polarity_score'] >= 0.05)
+            ]
+
+        # create a list of the values we want to assign for each condition
+        values = ['Negative', 'Neutral', 'Positive']
+
+        # create a new column and use np.select to assign values to it using our lists as arguments
+        df['sentiment_label'] = np.select(conditions, values)
     
     def _textblob(self, df, text_col):
         """
         Function to get sentiment label using vader library
         """
-        pass
+        #iterate through rows to get polarity score
+        for ix, row in df.iterrows():
+            df.loc[ix, 'polarity_score'] = round(TextBlob(row[text_col]).sentiment.polarity, 3)
+        
+        # create a list of our conditions
+        conditions = [
+            (df['polarity_score'] < 0),
+            (df['polarity_score'] == 0),
+            (df['polarity_score'] > 0)
+            ]
+
+        # create a list of the values we want to assign for each condition
+        values = ['Negative', 'Neutral', 'Positive']
+
+        # create a new column and use np.select to assign values to it using our lists as arguments
+        df['sentiment_label'] = np.select(conditions, values)
+            
+        
+    def _bert(self, df, text_col):
+        """
+        Function to get sentiment label using bert 
+        """
 
     def get_sentiment(self, df, text_col):
         if self.method == 'vader':
--- src/train.py
+++ src/train.py
@@ -1,2 +1,3 @@
 #train model - BERT - look into transfer learning
-
+# need to look for alternative - data set - ML with embeddings 
+#
