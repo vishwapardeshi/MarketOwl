03364b8af1648acc166a25777e67c101fcdca0b7
Tony Chu <tonychu.yw@gmail.com>
2021-05-14 02:42:48 -0700

    Update summarization.py

--- src/summarization.py
+++ src/summarization.py
@@ -10,18 +10,13 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 
 class Summarization:
 
-    #---------------------------------------------------------------------#
-
     def __init__(self, model='bart'):
-    """
-    Note: I think BART produces the best result, while the others do not make meaningful summaries.
-    """
+        """
+        Note: I think BART produces the best result, while the others do not make meaningful summaries.
+        """
+        self.model = model
 
-        self.model = model    # choose model: 't5', 'bart', 'pegasus', or 'led'
-
-    #---------------------------------------------------------------------#
-
-    def _summarize(self, text, method=self.model):
+    def _summarize(self, text):
         """
         Function to summarize a text
         (Note that this is different from other classes in this package: we only summarize
@@ -33,21 +28,21 @@ class Summarization:
         words = word_tokenize(text)
 
         # select model
-        if method == 't5':
+        if self.model == 't5':
             from transformers import T5Tokenizer, T5ForConditionalGeneration
             model_name = 't5-base'
-        elif method == 'bart':
+        elif self.model == 'bart':
             from transformers import BartTokenizer, BartForConditionalGeneration
             model_name = 'facebook/bart-large-cnn'
-        elif method == 'pegasus':
+        elif self.model == 'pegasus':
             from transformers import PegasusTokenizer, PegasusForConditionalGeneration
             model_name = "human-centered-summarization/financial-summarization-pegasus"
-        elif method == 'led':
+        elif self.model == 'led':
             from transformers import LEDTokenizer, LEDForConditionalGeneration, LEDConfig
             model_name = 'allenai/led-base-16384'
 
         # run seleted model
-        if method in ['t5', 'bart', 'pegasus']:
+        if self.model in ['t5', 'bart', 'pegasus']:
             # initialize model
             model = BartForConditionalGeneration.from_pretrained(model_name)
             tokenizer = BartTokenizer.from_pretrained(model_name)
@@ -68,7 +63,7 @@ class Summarization:
                 elif end > len(sentences):
                     text_to_summarize = ' '.join(sentences[begin:])
 
-                if method == 't5':
+                if self.model == 't5':
                     input_ids = tokenizer.encode("summarize: " + text_to_summarize, truncation=True, return_tensors="pt")
                 else:
                     input_ids = tokenizer(text_to_summarize, truncation=True, return_tensors="pt").input_ids
@@ -84,18 +79,20 @@ class Summarization:
                 begin += stride
                 end += stride
 
-        elif method == 'led':
+        elif self.model == 'led':
             model = LEDForConditionalGeneration.from_pretrained(model_name)
             tokenizer = LEDTokenizer.from_pretrained(model_name)
             inputs = tokenizer([text], truncation=True, return_tensors='pt')
             summary_ids = model.generate(inputs['input_ids'], num_beams=4, early_stopping=True)
             summary = [tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False) for g in summary_ids]
 
+        return summary
+
     #---------------------------------------------------------------------#
 
-    def get_summary(self, text, method=self.model):
-        if method in ['t5', 'bart', 'pegasus', 'led']:
-            summary = self._summarize(text, method)
+    def get_summary(self, text):
+        if self.model in ['t5', 'bart', 'pegasus', 'led']:
+            summary = self._summarize(text)
         else:
             raise ValueError("Incorrect model name for extracting keywords! Should be 't5', 'bart', 'pegasus', or 'led'.")
 
@@ -103,8 +100,8 @@ class Summarization:
 
     #---------------------------------------------------------------------#
 
-    def save_summary(summary, file_name, folder_name):
-        path = os.path.join(folder_name, file_name)
+    def save_summary(self, summary, file_name, destination):
+        path = os.path.join(destination, file_name)
         with open(path, "w") as text_file:
             text_file.write('* ' + ' \n* '.join(summary))
 
